9f02edf1a72cb4a2b15300977be7ac5765c6bd82
akshay <gravity.akshay@gmail.com>
2022-07-30 20:05:26 +0530

    Remove product as component and add product-list as component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProductComponent } from './product/product.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductComponent
+    ProductListComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit {
+  id: number = 1;
+  name: string = "Book";
+  price: number = 9.99;
+  url: string = "https://images.unsplash.com/photo-1544947950-fa07a98d237f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80";
+  description: string = "You can read it!";
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
