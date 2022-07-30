88ff2915ba4c62ad3fdf83620cd0bcbfbaecf465
akshay <gravity.akshay@gmail.com>
2022-07-31 00:13:08 +0530

    Add httpservice and pull data from data.json

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../services/product.service';
+import { HttpService } from '../services/http.service';
+import { ProductClass } from '../models/product-class.model';
 
 @Component({
   selector: 'app-product-list',
@@ -7,10 +9,13 @@ import { ProductService } from '../services/product.service';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
+  products: ProductClass[] = [];
 
-  constructor(private ProductService: ProductService) { }
+  constructor(private ProductService: ProductService, private HttpService: HttpService) { }
 
   ngOnInit(): void {
+    this.HttpService.getProducts().subscribe(data =>{
+      this.products = data;
+    })
   }
-
 }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { ProductClass } from '../models/product-class.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
+  productlist: ProductClass[]= [];
 
   constructor() { }
+
+  getProducts2(){
+    return this.productlist;
+  }
 }
