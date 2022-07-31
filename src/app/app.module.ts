2175c3bd93f2753751fa0b95531131b36126029c
akshay <gravity.akshay@gmail.com>
2022-07-31 14:08:34 +0530

    Landing page ready

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './product-list/product-list.component';
+import { ProductPageComponent } from './product-page/product-page.component';
+import { CartComponent } from './cart/cart.component';
+import { SubmitComponent } from './submit/submit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent
+    ProductListComponent,
+    ProductPageComponent,
+    CartComponent,
+    SubmitComponent
   ],
   imports: [
     BrowserModule,
--- src/app/models/product-class.model.ts
+++ src/app/models/product-class.model.ts
@@ -4,12 +4,14 @@ export class ProductClass {
     url: string;
     price: number;
     description: string;
+    count: number;
 
-    constructor(id: number, name: string, url: string, price: number, description: string){
-        this.id = id;
-        this.name = name;
-        this.price = price;
-        this.url = url;
-        this.description = description;
+    constructor(){
+        this.id = 0;
+        this.name = "";
+        this.price = 0;
+        this.url = "";
+        this.description = "";
+        this.count= 0;
     }
 }
