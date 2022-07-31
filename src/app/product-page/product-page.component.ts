b506a86210de1f413046731afd2f078de351ebfb
akshay <gravity.akshay@gmail.com>
2022-07-31 19:23:00 +0530

    Skeleton of product page ready

--- src/app/product-page/product-page.component.ts
+++ src/app/product-page/product-page.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { ProductService } from '../services/product.service';
+import { HttpService } from '../services/http.service';
+import { ProductClass } from '../models/product-class.model';
 
 @Component({
   selector: 'app-product-page',
@@ -6,10 +9,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-page.component.css']
 })
 export class ProductPageComponent implements OnInit {
-
-  constructor() { }
+  products: ProductClass[] = [];
+  myproduct: ProductClass[] = [];
+  id: number = 2;
+  constructor(private ProductService: ProductService, private HttpService: HttpService) { }
 
   ngOnInit(): void {
+    this.HttpService.getProducts().subscribe(data =>{
+      this.products = data;
+      this.products = this.products.filter(x => {return x.id == this.id});
+      this.myproduct[0] = this.products[0];
+    })
+    
+
   }
 
 }
