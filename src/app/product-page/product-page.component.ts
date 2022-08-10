7852096b6fe1f8f5da7661a626d532b28496d99b
akshay <gravity.akshay@gmail.com>
2022-08-10 22:47:14 +0530

    Add routing with data from product-list to product-list detail page

--- src/app/product-page/product-page.component.ts
+++ src/app/product-page/product-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { ProductService } from '../services/product.service';
 import { HttpService } from '../services/http.service';
 import { ProductClass } from '../models/product-class.model';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product-page',
@@ -11,13 +12,19 @@ import { ProductClass } from '../models/product-class.model';
 export class ProductPageComponent implements OnInit {
   products: ProductClass[] = [];
   myproduct: ProductClass[] = [];
-  id: number = 2;
-  constructor(private ProductService: ProductService, private HttpService: HttpService) { }
+  id: string = '0';
+  
+  constructor(private ProductService: ProductService, private HttpService: HttpService, private _Activatedroute:ActivatedRoute) { }
 
   ngOnInit(): void {
     this.HttpService.getProducts().subscribe(data =>{
       this.products = data;
-      this.products = this.products.filter(x => {return x.id == this.id});
+      this._Activatedroute.queryParams.subscribe(params=>{
+        this.id = params['id'];
+      });
+      const test:number = +this.id;
+      console.log(test);
+      this.products = this.products.filter(x => {return x.id == test});
       this.myproduct[0] = this.products[0];
     })
     
