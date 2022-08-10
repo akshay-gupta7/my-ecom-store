bf484b876151ad846057f5718bab1dba07b777c5
akshay <gravity.akshay@gmail.com>
2022-08-10 21:56:17 +0530

    Add Router routes and components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductPageComponent } from './product-page/product-page.component';
+import { CartComponent } from './cart/cart.component';
+import { SubmitComponent } from './submit/submit.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ProductListComponent },
+  { path: 'product-detail', component: ProductPageComponent },
+  { path: 'cart', component: CartComponent },
+  { path: 'final-submit', component: SubmitComponent  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
